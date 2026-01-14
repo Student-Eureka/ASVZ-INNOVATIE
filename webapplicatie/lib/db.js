cba36ce26431a18dc88126d39808fae20f1e02f3
Tim Bos <99076495@mydavinci.nl>
2026-01-14 18:22:55 +0100

    database update user

--- webapplicatie/lib/db.js
+++ webapplicatie/lib/db.js
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise";
 // Connection pools zorgen voor efficiënter gebruik van databaseverbindingen
 export const db = mysql.createPool({
   host: "localhost",        // Database host; verander dit naar je Docker host indien nodig
-  user: "root",             // Database gebruikersnaam
+  user: "asvz_user",             // Database gebruikersnaam
   password: "AsvzEureka2!",             // Wachtwoord van de gebruiker (LET OP: voor hosting NOOIT leeg houden, WACHTWOORD PRIVE HOUDEN)
   database: "asvz_db"   // Naam van de database die je wilt gebruiken
 });
