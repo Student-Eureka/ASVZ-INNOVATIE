824e328690ee3814575ed8c3601ca12e025710df
Tim Bos <99076495@mydavinci.nl>
2026-01-14 18:13:14 +0100

    database update

--- webapplicatie/lib/db.js
+++ webapplicatie/lib/db.js
@@ -5,6 +5,6 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "localhost",        // Database host; verander dit naar je Docker host indien nodig
   user: "root",             // Database gebruikersnaam
-  password: "",             // Wachtwoord van de gebruiker (LET OP: voor hosting NOOIT leeg houden, WACHTWOORD PRIVE HOUDEN)
-  database: "woningen_db"   // Naam van de database die je wilt gebruiken
+  password: "AsvzEureka2!",             // Wachtwoord van de gebruiker (LET OP: voor hosting NOOIT leeg houden, WACHTWOORD PRIVE HOUDEN)
+  database: "asvz_db"   // Naam van de database die je wilt gebruiken
 });
